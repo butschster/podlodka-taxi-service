df165984fb3ea2dfd983382d46654892f67f4d54
Pavel Buchnev <butschster@gmail.com>
2024-09-30 22:59:09 +0400

    Translate steps into ru

--- app/src/Endpoint/Temporal/Workflow/TaxiRequestWorkflow.php
+++ app/src/Endpoint/Temporal/Workflow/TaxiRequestWorkflow.php
@@ -8,22 +8,21 @@ final class TaxiRequestWorkflow
 {
     public function createRequest()
     {
-        // 1. First, we need to create a taxi request
+        // 1. Создать заявку на такси
 
-        // 2. Then we need to block money on user account for the ride
-        //   - If user doesn't have enough money, we can notify the user and cancel the request
+        // 2. Заблокировать стоимость поездки на счете клиента
+        // - Если у клиента недостаточно средств, то поехать не получится и нужно отменить заявку
+        // - Если что-то пойдет не так, то нужно вернуть деньги
 
-        // If something goes wrong, we need to refund the money
+        // 3. Уведомить водителей о новой заявке
+        // 4. Ждем максимум 5 минут, чтобы водитель принял заявку
+        // 5. Если водитель принял заявку, сначала валидируем водителя (класс, рейтинг и т. д.)
+        //   - Если водитель не подходит, ждем другого водителя
+        // 6. Назначаем водителя на заявку
+        // 7. Уведомляем клиента, что водитель принял заявку
 
-        // 3. Notify drivers about the new request
-        // 4. Wait max for 5 minutes for driver to accept the request
-        // 5. If driver is accepted, first validate the driver (class, rating, etc)
-        //   - If the driver is not matched, wait for another driver
-        // 6. Assign the driver to the request
-        // 7. Notify the user that the driver accepted the request
-
-        // 8. If the driver accepted the request, we can start the trip
-        // 9. Charge user for the trip
-        // 10. Notify user about the trip finish and ask to rate each other
+        // 8. Если водитель принял заявку, можем начать поездку
+        // 9. По завершению поездки списываем деньги с клиента
+        // 10. Уведомляем клиента о завершении поездки и предлагаем оценить друг водителя, а водителю - клиента
     }
 }
